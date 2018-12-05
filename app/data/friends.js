e36b38ea6f9dfec4f8b9730dbac82dbd9fc538cc
JoyceNguyen <jnguyen0581@gmail.com>
2018-12-04 23:07:10 -0600

    append photos

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Ariel',
+		photo: 'https://lumiere-a.akamaihd.net/v1/images/gallery_princess_ariel_5_28dcc9e3.jpeg?height=473&mode=crop&region=0%2C0%2C2181%2C1223&width=630',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'4',
+			'5',
+			'3',
+			'5',
+			'2',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Elsa',
+		photo: 'https://lumiere-a.akamaihd.net/v1/images/6d7454cea6644379adc7e529c5790a28078a2823.jpeg?region=0,0,450,450&width=320',
+		scores: [
+			'5',
+			'1',
+			'1',
+			'5',
+			'1',
+			'5',
+			'2',
+			'5',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Merida',
+		photo: 'https://lumiere-a.akamaihd.net/v1/images/gallery_princess_merida_8_72603f1f.jpeg?height=473&mode=crop&region=0%2C0%2C2181%2C1223&width=630',
+		scores: [
+			'5',
+			'1',
+			'1',
+			'4',
+			'4',
+			'2',
+			'5',
+			'2',
+			'2',
+			'5'
+		]
+	},
+	{
+		name: 'Jessie',
+		photo: 'https://vignette.wikia.nocookie.net/disney/images/7/7a/Jessie.png/revision/latest?cb=20131106225109',
+		scores: [
+			'3',
+			'1',
+			'2',
+			'4',
+			'5',
+			'1',
+			'4',
+			'3',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Minnie',
+		photo: 'https://www.pinterest.com/pin/471541023471222451/',
+		scores: [
+			'3',
+			'1',
+			'5',
+			'2',
+			'1',
+			'5',
+			'3',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Mickey',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'1',
+			'3',
+			'3',
+			'3',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Gaston',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/Gaston_smirking.jpg/330px-Gaston_smirking.jpg',
+		scores: [
+			'5',
+			'2',
+			'1',
+			'5',
+			'4',
+			'1',
+			'5',
+			'1',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Hercules',
+		photo: 'https://vignette.wikia.nocookie.net/disney/images/a/a0/Hercules_%28character%29.jpg/revision/latest/scale-to-width-down/126?cb=20120718073554',
+		scores: [
+			'4',
+			'5',
+			'5',
+			'1',
+			'5',
+			'1',
+			'3',
+			'3',
+			'5',
+			'2'
+		]
+	},
+	{
+		name: 'Beast',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/e/ee/Beast_%28Disney%29.jpg',
+		scores: [
+			'5',
+			'1',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Simba',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/YoungSimbaSmilestlk.png/330px-YoungSimbaSmilestlk.png',
+		scores: [
+			'4',
+			'3',
+			'1',
+			'5',
+			'1',
+			'4',
+			'5',
+			'4',
+			'3',
+			'3'
+		]
+	}
+];
+
+module.exports = friendsArray;
